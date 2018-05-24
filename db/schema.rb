79855ad33b5c59c742ccac2e4dfe741046fd3f4d
Ashleigh Maund <ashleighmaund@gmail.com>
2018-05-24 14:12:32 +0100

    ran bundle install, rake db:create and rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180524072027) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "items", force: :cascade do |t|
     t.string   "item"
     t.datetime "created_at", null: false
